9541ce85d8d50b8515536908101d7501ff989945
ylio <ylio@DESKTOP-MAO6E4J>
2025-12-18 15:56:17 +0100

    fix commit

--- js/site.js
+++ js/site.js
@@ -1,21 +1,50 @@
-$("#calculateBtn").click(function() {
-    const firstNumber = $("#firstNumberInput").val();
-    const secondNumber = $("#secondNumberInput").val();
-    const operation = $("#operatorSelect").val();
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
+let enteringSecond = false;
 
+$(".num").click(function() {
+    if (!enteringSecond) {
+        firstNumber += $(this).text();
+        $(".wendow").text(firstNumber);
+    } else {
+        secondNumber += $(this).text();
+        $(".wendow").text(secondNumber);
+    }
+});
+
+$(".op").click(function() {
+    operator = $(this).text();
+    enteringSecond = true;
+    $(".wendow").text("");
+});
+
+$(".clear").click(function() {
+    firstNumber = "";
+    secondNumber = "";
+    operator = "";
+    enteringSecond = false;
+    $(".wendow").text("");
+});
+
+$(".result").click(function() {
     $.ajax({
-        url: "https://backendkalkulator3-d0c4fdefdbe2ahe6.polandcentral-01.azurewebsites.net/calculate", // 👈 pamiętaj endpoint
+        url: "https://backendkalkulator3-d0c4fdefdbe2ahe6.polandcentral-01.azurewebsites.net/calculate",
         method: "POST",
         contentType: "application/json",
         data: JSON.stringify({
             a: firstNumber,
             b: secondNumber,
-            operator: operation
+            operator: operator
         }),
-        success: function (res) {
+        success: function(res) {
             $(".wendow").text(res.result);
+            firstNumber = res.result; // wynik staje się nowym pierwszym numerem
+            secondNumber = "";
+            operator = "";
+            enteringSecond = false;
         },
-        error: function (err) {
+        error: function(err) {
             console.error(err);
             alert("Błąd backendu");
         }
