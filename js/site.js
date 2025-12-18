4aa7548625fdf9c453b6e59f693f59675a4f818b
ylio <ylio@DESKTOP-MAO6E4J>
2025-12-18 15:45:58 +0100

    fix commit

--- js/site.js
+++ js/site.js
@@ -1,18 +1,23 @@
-$.ajax({
-    url: "https://backendkalkulator3-d0c4fdefdbe2ahe6.polandcentral-01.azurewebsites.net",
+$("#calculateBtn").click(function() {
+    const firstNumber = $("#firstNumberInput").val();
+    const secondNumber = $("#secondNumberInput").val();
+    const operation = $("#operatorSelect").val();
 
-    method: "POST",
-    contentType: "application/json",
-    data: JSON.stringify({
-        a: firstNumber,
-        b: secondNumber,
-        operator: operation
-    }),
-    success: function (res) {
-        $(".wendow").text(res.result);
-    },
-    error: function (err) {
-        console.error(err);
-        alert("Błąd backendu");
-    }
+    $.ajax({
+        url: "https://backendkalkulator3-d0c4fdefdbe2ahe6.polandcentral-01.azurewebsites.net/calculate", // 👈 pamiętaj endpoint
+        method: "POST",
+        contentType: "application/json",
+        data: JSON.stringify({
+            a: firstNumber,
+            b: secondNumber,
+            operator: operation
+        }),
+        success: function (res) {
+            $(".wendow").text(res.result);
+        },
+        error: function (err) {
+            console.error(err);
+            alert("Błąd backendu");
+        }
+    });
 });
